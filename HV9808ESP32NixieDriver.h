6b22b40ddaadaedd609417fb9349ab0a0d1a1acb
Paul Andrews <judge2005@gmail.com>
2022-11-20 11:55:15 -0500

    Add HV9808 6 digit drivers

---
+++ HV9808ESP32NixieDriver.h
@@ -0,0 +1,71 @@
+/*
+ * HV9808NixieDriver.h
+ *
+ *  Created on: Dec 3, 2017
+ *      Author: Paul Andrews
+ */
+
+#ifndef LIBRARIES_NIXIEDRIVER_HV9808ESP32NIXIEDRIVER_H_
+#define LIBRARIES_NIXIEDRIVER_HV9808ESP32NIXIEDRIVER_H_
+
+#include <NixieDriver.h>
+#include <SPI.h>
+
+class HV9808ESP32NixieDriver : public NixieDriver {
+public:
+	HV9808ESP32NixieDriver(int LEpin) : LEpin(LEpin) {}
+	HV9808ESP32NixieDriver(int LEpin, bool small) : LEpin(LEpin), small(small) {}
+	~HV9808ESP32NixieDriver() {}
+
+	virtual void setAnimation(Animation animation, int direction);
+	virtual bool supportsAnimation() { return true; }
+	virtual bool animationDone();
+	virtual void init();
+
+protected:
+	int LEpin;
+	bool small = false;
+	static DRAM_CONST const uint32_t nixieDigitMap[16];
+	static DRAM_CONST const uint32_t colonMap[6];
+	static uint32_t currentColonMap[6];	// Not used
+
+	static DRAM_CONST const uint32_t dp1 = 0x100000;
+	static DRAM_CONST const uint32_t dp2 = 0x200000;
+
+	uint8_t getSPIMode() { return SPI_MODE0; }
+	virtual void cacheColonMap();
+
+	uint32_t NIXIE_DRIVER_ISR_FLAG getMultiplexPins();
+	uint32_t NIXIE_DRIVER_ISR_FLAG getPins(byte mask);
+	uint32_t NIXIE_DRIVER_ISR_FLAG getPin(uint32_t digit);
+	uint32_t NIXIE_DRIVER_ISR_FLAG convertPolarity(uint32_t pins);
+	void NIXIE_DRIVER_ISR_FLAG esp32InterruptHandler();
+	bool NIXIE_DRIVER_ISR_FLAG esp32CalculateFade(uint32_t nowMs);
+	bool NIXIE_DRIVER_ISR_FLAG adjustAnimation(uint32_t nowMs);
+
+	// Never called, but we need to define it
+	virtual bool NIXIE_DRIVER_ISR_FLAG calculateFade(uint32_t nowMs);
+	virtual void NIXIE_DRIVER_ISR_FLAG interruptHandler();
+
+private:
+	volatile Animation animation = ANIMATION_NONE;
+	volatile int animatedDigit = 0;
+	volatile int direction = 0;
+	volatile uint32_t startAnimation = 0;
+	SoftPWM animatorPWM = SoftPWM(0, 3);
+
+	static volatile uint32_t callCycleCount;
+	static HV9808ESP32NixieDriver *_handler;
+#ifdef ESP32
+	static hw_timer_t *timer;
+#endif
+
+
+	// Can't be using any system calls in case some other
+	// weird system calls are being called, because they can do weird
+	// things with flash. For the same reason, this function needs to be
+	// in RAM.
+	static NIXIE_DRIVER_ISR_FLAG void isr();
+};
+
+#endif /* LIBRARIES_NIXIEDRIVER_HV9808ESP32NIXIEDRIVER_H_ */
